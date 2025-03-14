6afa2aca68ed28dc3851c08ef754e7a132acd349
Akhil <msakhilvinayak@gmail.com>
2025-03-14 11:02:27 +0530

    feat: fast api v1

---
+++ app/schemas/predict.py
@@ -0,0 +1,33 @@
+from typing import Any, List, Optional
+
+from pydantic import BaseModel
+
+
+class PredictionResults(BaseModel):
+    errors: Optional[Any]
+    version: str
+    #predictions: Optional[List[int]]
+    predictions: Optional[float]
+
+class DataInputSchema(BaseModel):
+    month: Optional[int]
+    year: Optional[int]
+    season: Optional[str]
+    hr: Optional[str]
+    holiday: Optional[str]
+    weekday: Optional[str]
+    workingday: Optional[str]
+    weathersit: Optional[str]
+    temp: Optional[float]
+    atemp: Optional[float]
+    hum: Optional[float]
+    windspeed: Optional[float]
+    casual: Optional[int]
+    registered: Optional[int]
+    # cnt: Optional[int]
+    dteday: Optional[str]
+
+
+class MultipleDataInputs(BaseModel):
+    inputs: List[DataInputSchema]
+
